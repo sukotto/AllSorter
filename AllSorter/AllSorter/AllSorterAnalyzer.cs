56e9dc37d762363669b3d062fae7a15dcda3feb3
sukotto <sukottoburaun@gmail.com>
2020-05-02 10:52:58 +1000

    Add project files.

---
+++ AllSorter/AllSorter/AllSorterAnalyzer.cs
@@ -0,0 +1,63 @@
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AllSorter
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AllSorterAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AllSorter";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.ClassAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.ClassAnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.ClassAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Sorting";
+
+        private static DiagnosticDescriptor ClassSortRule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(ClassSortRule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            //context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxTreeAction(AnalyseTreeForUnsortedClasses);
+        }
+
+        public static void AnalyseTreeForUnsortedClasses(SyntaxTreeAnalysisContext context)
+        {
+            string fileName = context.Tree.FilePath.Split('\\').Last();
+            SyntaxNode syntaxNode = context.Tree.GetRoot();
+            IEnumerable<SyntaxNode> descendantNodes = syntaxNode.DescendantNodes();
+            List<ClassDeclarationSyntax> classes = descendantNodes.OfType<ClassDeclarationSyntax>().ToList();
+            if (classes.Count == 0)
+            {
+                return;
+            }
+
+            List<ClassDeclarationSyntax> sortedClasses = classes.OrderBy(x => x.Identifier.ValueText).ToList();
+
+            for(int i = 0; i < classes.Count; i++)
+            {
+                var classDeclarationSyntax = classes[i];
+                if(classDeclarationSyntax.ToString() != sortedClasses[i].ToString())
+                {
+                    Diagnostic diagnostic = Diagnostic.Create(ClassSortRule, classDeclarationSyntax.GetLocation(), fileName);
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+
+        }
+    }
+}
---
+++ AllSorter/AllSorter/AllSorterCodeFixProvider.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace AllSorter
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AllSorterCodeFixProvider)), Shared]
+    public class AllSorterCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Sort classes";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(AllSorterAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            //var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            foreach (Diagnostic diagnostic in context.Diagnostics.Where(d => FixableDiagnosticIds.Contains(d.Id)))
+            {
+
+                // Register a code action that will invoke the fix.
+                context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: title,
+                    createChangedDocument: c => GetSortedClassDocumentAsync(context.Document, diagnostic, c),
+                    equivalenceKey: title),
+                diagnostic);
+
+            }
+
+            await Task.FromResult(Task.CompletedTask);
+        }
+
+        private static async Task<Document> GetSortedClassDocumentAsync(Document document, Diagnostic diagnostic, CancellationToken cancellationToken)
+        {
+            try
+            {
+                SyntaxNode root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+                List<ClassDeclarationSyntax> classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+                List<ClassDeclarationSyntax> sortedClasses = classes.OrderBy(x => x.Identifier.ValueText).ToList();
+
+                DocumentEditor documentEditor = await DocumentEditor.CreateAsync(document, cancellationToken);
+                for (var index = 0; index < classes.Count; index++)
+                {
+                    ClassDeclarationSyntax aClass = classes[index];
+                    ClassDeclarationSyntax aSortedClass = sortedClasses[index];
+
+                    /* Using aSortedClass directly in ReplaceNode would cause an error. See https://github.com/dotnet/roslyn/issues/37226 */
+                    ClassDeclarationSyntax copyOfSortedClass = SyntaxFactory.ClassDeclaration(attributeLists: aSortedClass.AttributeLists,
+                        modifiers: aSortedClass.Modifiers, identifier: aSortedClass.Identifier, typeParameterList: aSortedClass.TypeParameterList,
+                        baseList: aSortedClass.BaseList, constraintClauses: aSortedClass.ConstraintClauses, members: aSortedClass.Members)
+                        .WithLeadingTrivia(aSortedClass.GetLeadingTrivia())
+                        .WithTrailingTrivia(aSortedClass.GetTrailingTrivia());
+
+                    documentEditor.ReplaceNode(aClass, copyOfSortedClass);
+                }
+                return documentEditor.GetChangedDocument();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return document;
+            }
+        }
+    }
+}
