0bfae1cac9b0c7eb6acdf48e1f39a13b58341bf6
sukotto <sukottoburaun@gmail.com>
2020-05-03 11:21:33 +1000

    Added a unit test for the class sorting code fix

--- AllSorter/AllSorter.Test/AllSorterUnitTests.cs
+++ AllSorter/AllSorter.Test/AllSorterUnitTests.cs
@@ -1,10 +1,7 @@
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using TestHelper;
-using AllSorter;
 
 namespace AllSorter.Test
 {
@@ -12,62 +9,70 @@ namespace AllSorter.Test
     public class UnitTest : CodeFixVerifier
     {
 
-        //No diagnostics expected to show up
+        //Tests CodeFix for sorting classes
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSortClasses()
         {
-            var test = @"";
+            var test = @"using System;
 
-            VerifyCSharpDiagnostic(test);
-        }
-
-        //Diagnostic and CodeFix both triggered and checked for
-        [TestMethod]
-        public void TestMethod2()
+namespace Anteaters
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TypeName
-        {   
+            
         }
-    }";
-            var expected = new DiagnosticResult
-            {
-                Id = "AllSorter",
-                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-                Severity = DiagnosticSeverity.Warning,
-                Locations =
-                    new[] {
-                            new DiagnosticResultLocation("Test0.cs", 11, 15)
-                        }
-            };
+    }
 
-            VerifyCSharpDiagnostic(test, expected);
-
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
+    class Pangolin
     {
-        class TYPENAME
-        {   
+        public void PangolinMethod()
+        {
+
         }
-    }";
+    }
+
+    class Echidna
+    {
+        public void EchidnaMethod()
+        {
+
+        }
+    }
+}";
+            
+            var fixtest = @"using System;
+
+namespace Anteaters
+{
+
+    class Echidna
+    {
+        public void EchidnaMethod()
+        {
+
+        }
+    }
+
+    class Pangolin
+    {
+        public void PangolinMethod()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}";
             VerifyCSharpFix(test, fixtest);
         }
+
 
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
